4d06f03e1a0bd2ffd16facaedb1cca472faf7f39
robin <robin.cobanov@hotmail.com>
2023-03-16 17:45:57 +0100

    Now renders seats. Not getting 2darray to work with Hooks

--- src/pages/SelectSeats.jsx
+++ src/pages/SelectSeats.jsx
@@ -1,46 +1,66 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import './SelectSeats.css';
 
 export default function SelectSeats(props) {
   const {screeningid, auditoriumId, adultTickets, seniorTickets, childrenTickets} = useParams();
   const auditoriumSeats = props.auditoriumSeats
-  let numberOfRows;
 
+  const [numberOfRows, setNumberOfRows] = useState(0);
+  //const [twoDArray, setTwoDArray] = useState([])
+  //let numberOfRows;
   const screeningAuditoriumSeats = auditoriumSeats.filter(seat => seat.auditoriumId === parseInt(auditoriumId))
-  if (parseInt(auditoriumId) === 1) numberOfRows = 9;
-  if (parseInt(auditoriumId) === 2) numberOfRows = 6;
+  useEffect(() => {
+    if (parseInt(auditoriumId) === 1) setNumberOfRows(9);
+    if (parseInt(auditoriumId) === 2) setNumberOfRows(6);
+  }, [])
+  //const screeningAuditoriumSeats = auditoriumSeats.filter(seat => seat.auditoriumId === parseInt(auditoriumId))
+  //if (parseInt(auditoriumId) === 1) numberOfRows = 9;
+  //if (parseInt(auditoriumId) === 2) numberOfRows = 6;
+  /*
+  useEffect(() => {
+    setTwoDArray(create2DArray())
+    console.log(twoDArray)
+  },[occupiedSeats])*/
+  
+  const [occupiedSeats, setOccupiedSeats] = useState('');
 
-
+  useEffect(() => {
+    fetch(`/api/occupied_seats?screeningid=${screeningid}}`)
+      .then(result => result.json())
+      .then(data => setOccupiedSeats(data))
+      .catch(error=> console.error((error)))
+  }, [])
+  
   //const [twoDArray, setTwoDArray] = useState([]);
-   const twoDArray = create2DArray();
+  const twoDArray = create2DArray();
   //setTwoDArray(create2DArray());
   
   function create2DArray() {
     let twoDArray = [];
-    let tmpArray = [];
-    for (let i = 1; i < numberOfRows; i++){
-      tmpArray = [];
-      for (let y = 1; y < 10; y++){
-        const seat = screeningAuditoriumSeats.find(row => row.rowNumber === i)
-        if(typeof seat !== 'undefined') tmpArray.push(seat)
-      }
-      twoDArray.push(tmpArray);
+    for (let i = 1; i <= numberOfRows; i++){
+      const seats = screeningAuditoriumSeats.filter(x => x.rowNumber === i);
+      twoDArray.push(seats);
     }
-    console.log(twoDArray);
     return twoDArray;
   }
 
-//{screeningAuditoriumSeats.map(seat => <p>{seat.seatNumber}</p>)}
-//      
-
+//
   return (
   
     <>
       <h1>Select Seats</h1>
-      <p>{console.log(twoDArray)}</p>
-
-      
-
+      <div className="seats">
+      {twoDArray.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((seat, seatIndex) => (
+            <div key={seatIndex} className={`seat ${occupiedSeats[0]?.occupiedSeats.includes(seat.seatNumber) ? 'occupied' : 'available' }`}>
+              {seat.seatNumber}
+            </div>
+          ))}
+        </div>
+      ))}
+      </div>    
     </>
   )
 }
