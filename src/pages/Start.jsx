b68399680cecc18db5bd9168b002e45bb0ee6308
robin <robin.cobanov@hotmail.com>
2023-03-20 16:08:53 +0100

    Final commit.

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -9,7 +9,7 @@ export default function Start(props) {
     <>
       <h1>Välkommen till Feature Flicks!</h1>
         <Row>
-        <p>Vårt mål är att skapa en bekväm och avkopplande miljö för dig att avnjuta en film i. Välkommen in i våra moderna salonger med den senaste ljud- och bildtekniken och låt dig slukas utav förstklassig filmupplevelse. På Feature Flicks så har vi också en kiosk där du kan köpa popcorn, läsk och andra snacks för att göra din bioupplevelse ännu mer komplett. Varmt välkommen att boka din upplevelse via vår bokningssida!
+        <p>Vårt mål är att skapa en bekväm och avkopplande miljö för dig att avnjuta en film i. Välkommen in i våra moderna salonger med den senaste ljud- och bildtekniken och låt dig slukas utav förstklassig filmupplevelse. På Feature Flicks så har vi också en kiosk där du kan köpa popcorn, läsk och andra snacks för att göra din bioupplevelse ännu mer komplett. Varmt välkommen att boka din bioupplevelse via vår bokningssida!
           </p>
         </Row>
       <h3>Aktuella filmer</h3>
